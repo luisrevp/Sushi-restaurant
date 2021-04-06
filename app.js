55c1284a631a588363f5ac9d8603ab8dd7b7fc25
Luis Revilla <luisrev97@gmail.com>
2021-04-06 00:44:42 -0300

    Realizado cambios en ficheros, barra de nav responsive

--- app.js
+++ app.js
@@ -1,11 +1,57 @@
-let contImagenes = document.getElementsByClassName("img-container");
-let texto = document.createElement("p");
-let imagen = document.querySelectorAll(".imagen");
+// seleccionando el NAV. Creando el media query
+const navContainer = document.querySelector(".nav-container");
+const ulContainer = document.querySelector(".ul-container");
+const resMobile = matchMedia("(max-width: 670px)");
+// funcion
+const displayNav = (mql) => {
+  mql.matches
+    ? (ulContainer.style.display = "none")
+    : (ulContainer.style.display = "flex") &&
+      imgBurger.setAttribute("src", "./media/Hamburger.svg");
+};
+// agregando el evento
+resMobile.addListener(displayNav);
+// llamando la función al iniciar la página
+displayNav(resMobile);
 
-texto.innerHTML = "ORDENA YA!";
-texto.style="position: absolute; color: #fff;top: 50%;bottom: 50%;left: 50%;right: 50%;z-index: 50;"
-console.log(texto);
-console.log(contImagenes);
-console.log(imagen);
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
-// contImagenes[0].insertBefore(texto, imagen[0]);
+// burger button
+
+const burgerButton = document.querySelector(".burger-container");
+const imgBurger = document.querySelector(".burger-container img");
+
+console.log(imgBurger);
+console.log(burgerButton);
+const showNav = () => {
+  ulContainer.style.display == "none"
+    ? (ulContainer.style = "display:flex") &&
+      imgBurger.setAttribute("src", "./media/cross.svg")
+    : (ulContainer.style.display = "none") &&
+      imgBurger.setAttribute("src", "./media/Hamburger.svg");
+};
+burgerButton.addEventListener("click", showNav);
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+const imgContainer = document.querySelectorAll(".img-container");
+console.log(imgContainer);
+const newCont = imgContainer.forEach((container) => {
+  console.log(container);
+});
