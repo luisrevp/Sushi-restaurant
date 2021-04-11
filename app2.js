9d1facab3b26ec512dea71991b9673702708bdb4
Luis Revilla <luisrev97@gmail.com>
2021-04-11 01:32:14 -0300

    acondicionado archivos app2.js y CSS

--- app2.js
+++ app2.js
@@ -1,51 +1,40 @@
 function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -34.6083, lng: -58.3712 };
-  // The map, centered at Uluru
+  // Coordenadas principales
+  const uluru = { lat: -34.5417, lng: -58.4679 };
+  // Insertando el mapa
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
+    zoom: 16,
     center: uluru,
   });
 
   // Ubicación de los locales
-  // LOCAL DE NUÑEZ
-  const localNunez = new google.maps.Marker({
-    position: {
-      lat: -34.54179156359598,
-      lng: -58.467931365380664,
-    },
-    map: map,
-  });
-  // LOCAL DE COLEGIALES
-  const localColegiales = new google.maps.Marker({
-    position: {
-      lat: -34.578348778131584,
-      lng: -58.44891855825333,
-    },
-    map: map,
-  });
-  // LOCAL DE CABALLITO
-  const localCaballito = new google.maps.Marker({
-    position: {
-      lat: -34.617117207892896,
-      lng: -58.43383389269026,
-    },
-    map: map,
-  });
-  // LOCAL DE VICENTE LOPEZ
-  const localVteLopez = new google.maps.Marker({
-    position: {
-      lat: -34.49916957515628,
-      lng: -58.494618999972204,
-    },
-    map: map,
-  });
-  // LOCAL DE LA MATANZA
-  const localMatanza = new google.maps.Marker({
-    position: {
-      lat: -34.68128414558699,
-      lng: -58.56030903830062,
-    },
-    map: map,
-  });
+  // Funcion que define el template para cada iniciación de marcador
+  const nuevoMarcador = (lugar) => {
+    let marcador = new google.maps.Marker({
+      position: lugar.coordenadas,
+      map: map,
+    });
+    if (lugar.content) {
+      let infoWindow = new google.maps.infoWindow({
+        content: lugar.content,
+      });
+      marcador.addListener("click", () => infoWindow.open(map, marcador));
+    }
+  };
+
+  //ARRAY DE LOCALES
+  let locales = [
+    // local Nuñez
+    { coordenadas: { lat: -34.54179156359598, lng: -58.467931365380664 } },
+    // local Colegiales
+    { coordenadas: { lat: -34.578348778131584, lng: -58.44891855825333 } },
+    // local Caballito
+    { coordenadas: { lat: -34.617117207892896, lng: -58.43383389269026 } },
+    // local Vte. Lopez
+    { coordenadas: { lat: -34.49916957515628, lng: -58.494618999972204 } },
+    // local La Matanza
+    { coordenadas: { lat: -34.68128414558699, lng: -58.56030903830062 } },
+  ];
+
+  locales.forEach((local) => nuevoMarcador(local));
 }
